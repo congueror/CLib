52491c3acef8dbec02c46138b2aa7129dbc46c10
congueror <38083828+congueror@users.noreply.github.com>
2020-07-26 17:37:17 +0300

    Update

---
+++ src/main/java/com/congueror/clib/blocks/MetalBlockItem.java
@@ -0,0 +1,12 @@
+package com.congueror.clib.blocks;
+
+import net.minecraft.block.Block;
+
+public class MetalBlockItem extends Block
+{
+
+	public MetalBlockItem(Properties properties) 
+	{
+		super(properties);
+	}
+}
--- src/main/java/com/congueror/clib/init/BlockInit.java
+++ src/main/java/com/congueror/clib/init/BlockInit.java
@@ -1,11 +1,7 @@
 package com.congueror.clib.init;
 
 import com.congueror.clib.ConguerorLib;
-import com.congueror.clib.blocks.AluminumBlock;
-import com.congueror.clib.blocks.CopperBlock;
-import com.congueror.clib.blocks.SteelBlock;
-import com.congueror.clib.blocks.TinBlock;
-
+import com.congueror.clib.blocks.MetalBlockItem;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
@@ -20,10 +16,10 @@ public class BlockInit
 	public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, ConguerorLib.MOD_ID);
     
     public static final RegistryObject<Block> TIN_ORE = BLOCKS.register("tin_ore", ()-> new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(1.2f, 1.2f).sound(SoundType.STONE).harvestTool(ToolType.PICKAXE).harvestLevel(1).setRequiresTool()));
-    public static final RegistryObject<Block> TIN_BLOCK = BLOCKS.register("tin_block", () -> new TinBlock(Block.Properties.create(Material.IRON).hardnessAndResistance(5f, 6f).sound(SoundType.METAL).harvestTool(ToolType.PICKAXE).harvestLevel(1).setRequiresTool()));
-    public static final RegistryObject<Block> STEEL_BLOCK = BLOCKS.register("steel_block", () -> new SteelBlock(Block.Properties.create(Material.IRON).hardnessAndResistance(4.5f, 12f).sound(SoundType.METAL).harvestTool(ToolType.PICKAXE).harvestLevel(1).setRequiresTool()));
+    public static final RegistryObject<Block> TIN_BLOCK = BLOCKS.register("tin_block", () -> new MetalBlockItem(Block.Properties.create(Material.IRON).hardnessAndResistance(5f, 6f).sound(SoundType.METAL).harvestTool(ToolType.PICKAXE).harvestLevel(1).setRequiresTool()));
+    public static final RegistryObject<Block> STEEL_BLOCK = BLOCKS.register("steel_block", () -> new MetalBlockItem(Block.Properties.create(Material.IRON).hardnessAndResistance(4.5f, 12f).sound(SoundType.METAL).harvestTool(ToolType.PICKAXE).harvestLevel(1).setRequiresTool()));
     public static final RegistryObject<Block> ALUMINUM_ORE = BLOCKS.register("aluminum_ore", () -> new Block(Block.Properties.create(Material.IRON).hardnessAndResistance(2.8f, 4f).sound(SoundType.STONE).harvestTool(ToolType.PICKAXE).harvestLevel(2).setRequiresTool()));
-    public static final RegistryObject<Block> ALUMINUM_BLOCK = BLOCKS.register("aluminum_block", () -> new AluminumBlock(Block.Properties.create(Material.IRON).hardnessAndResistance(5f, 6f).sound(SoundType.METAL).harvestTool(ToolType.PICKAXE).harvestLevel(2).setRequiresTool()));
+    public static final RegistryObject<Block> ALUMINUM_BLOCK = BLOCKS.register("aluminum_block", () -> new MetalBlockItem(Block.Properties.create(Material.IRON).hardnessAndResistance(5f, 6f).sound(SoundType.METAL).harvestTool(ToolType.PICKAXE).harvestLevel(2).setRequiresTool()));
     public static final RegistryObject<Block> COPPER_ORE = BLOCKS.register("copper_ore", () -> new Block(Block.Properties.create(Material.IRON).hardnessAndResistance(5f, 6f).sound(SoundType.STONE).harvestTool(ToolType.PICKAXE).harvestLevel(1).setRequiresTool()));
-    public static final RegistryObject<Block> COPPER_BLOCK = BLOCKS.register("copper_block", () -> new CopperBlock(Block.Properties.create(Material.IRON).hardnessAndResistance(1.5f, 1.5f).sound(SoundType.METAL).harvestTool(ToolType.PICKAXE).harvestLevel(1).setRequiresTool()));
+    public static final RegistryObject<Block> COPPER_BLOCK = BLOCKS.register("copper_block", () -> new MetalBlockItem(Block.Properties.create(Material.IRON).hardnessAndResistance(1.5f, 1.5f).sound(SoundType.METAL).harvestTool(ToolType.PICKAXE).harvestLevel(1).setRequiresTool()));
 }
