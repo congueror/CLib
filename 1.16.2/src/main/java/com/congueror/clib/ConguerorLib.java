68c2aa346c76031aec33c9b8ed9caf890feba21b
brain.exe <mariovionikos@gmail.com>
2020-09-05 02:03:01 +0300

    Worldgen or something

--- 1.16.2/src/main/java/com/congueror/clib/ConguerorLib.java
+++ 1.16.2/src/main/java/com/congueror/clib/ConguerorLib.java
@@ -1,59 +1,62 @@
 package com.congueror.clib;
 
+import org.apache.logging.log4j.LogManager;
+import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
+import net.minecraftforge.fml.event.server.FMLServerAboutToStartEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import com.congueror.clib.world.gen.ModOreGen;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.registries.IForgeRegistry;
 import net.minecraft.item.BlockItem;
+import com.congueror.clib.util.ClibItemGroup;
+import net.minecraftforge.fml.RegistryObject;
 import net.minecraft.item.Item;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.RegistryObject;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.IForgeRegistry;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-import com.congueror.clib.config.ClibConfig;
-import com.congueror.clib.init.BlockInit;
+import net.minecraftforge.common.MinecraftForge;
 import com.congueror.clib.init.ContainerTypes;
-import com.congueror.clib.init.ItemInit;
 import com.congueror.clib.init.TileEntityTypes;
-import com.congueror.clib.util.ClibItemGroup;
-import com.congueror.clib.world.gen.ModOreGen;
+import com.congueror.clib.init.BlockInit;
+import com.congueror.clib.init.ItemInit;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import com.congueror.clib.config.ClibConfig;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.ModLoadingContext;
+import org.apache.logging.log4j.Logger;
+import net.minecraftforge.fml.common.Mod;
 
 @Mod("clib")
-@Mod.EventBusSubscriber(modid = ConguerorLib.MOD_ID, bus = Bus.MOD)
+@Mod.EventBusSubscriber(modid = "clib", bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ConguerorLib
 {
-    public static final Logger LOGGER = LogManager.getLogger();
-    public static final String MOD_ID= "clib";
+    public static final Logger LOGGER;
+    public static final String MOD_ID = "clib";
     public static ConguerorLib instance;
-
-    public ConguerorLib() 
-    {    	
-    	final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-    	modEventBus.addListener(this::commonSetup);
-    	modEventBus.addListener(this::clientRegistries);
-    	modEventBus.addListener(this::doClientStuff);
-        instance=this;
+    
+    public ConguerorLib() {
+    	
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, ClibConfig.spec);
         
-        ClibConfig.init();
+        final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         
+        modEventBus.addListener(this::commonSetup);
+        modEventBus.addListener(this::clientRegistries);
+        modEventBus.addListener(this::doClientStuff);
+        
+        
+        ConguerorLib.instance = this;
         ItemInit.ITEMS.register(modEventBus);
         BlockInit.BLOCKS.register(modEventBus);
         TileEntityTypes.TILE_ENTITY_TYPES.register(modEventBus);
         ContainerTypes.CONTAINER_TYPES.register(modEventBus);
-        //RecipeSerializerInit.RECIPE_SERIALIZERS.register(modEventBus);
-                
+        
+        
+        
         MinecraftForge.EVENT_BUS.register(this);
         
-        ClibConfig.load();
+        
     }
     
     @SubscribeEvent
@@ -73,31 +76,32 @@ public class ConguerorLib
      }
     
     @SubscribeEvent
-    public void commonSetup(final FMLCommonSetupEvent event)
-    {
+    public void commonSetup(final FMLCommonSetupEvent event) {
+    }
+    
+    private void clientRegistries(final FMLClientSetupEvent event) {
+    }
+    
+    @SuppressWarnings("resource")
+	private void doClientStuff(final FMLClientSetupEvent event) {
+    	LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().gameSettings);
+    }
+    
+    @SubscribeEvent
+    public void onServerStarting(final FMLServerStartingEvent event) {
+    }
+    
+    @SubscribeEvent
+    public void loadComplete(final FMLLoadCompleteEvent event) {
+    	
+    }
+    
+    @SubscribeEvent
+    public void aboutToStart(final FMLServerAboutToStartEvent event) {
     	ModOreGen.addFeatures();
     }
     
-    private void clientRegistries(final FMLClientSetupEvent event) 
-    {
-    	
-    }
-    
-	@SuppressWarnings("resource")
-	private void doClientStuff(final FMLClientSetupEvent event) 
-    {
-        LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().gameSettings);
-    }
-
-    @SubscribeEvent
-    public void onServerStarting(FMLServerStartingEvent event) 
-    {
-    	
-    }
-    
-    @SubscribeEvent
-    public void loadComplete(FMLLoadCompleteEvent event) 
-    {  
-        
+    static {
+        LOGGER = LogManager.getLogger();
     }
 }
--- 1.16.2/src/main/java/com/congueror/clib/config/ClibConfig.java
+++ 1.16.2/src/main/java/com/congueror/clib/config/ClibConfig.java
@@ -1,93 +1,84 @@
 package com.congueror.clib.config;
 
-import com.congueror.clib.ConguerorLib;
-import com.electronwill.nightconfig.core.file.CommentedFileConfig;
-import com.electronwill.nightconfig.core.io.WritingMode;
-
-import net.minecraftforge.common.ForgeConfigSpec;
-import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.loading.FMLPaths;
+import net.minecraftforge.common.ForgeConfigSpec;
 
-public class ClibConfig 
-{   
-    public static ForgeConfigSpec clientSpec;
-    public static ForgeConfigSpec serverSpec;
-    
+public class ClibConfig
+{
+    public static final String CATEGORY_GENERAL = "General";
+    public static final String CATEGORY_OREGEN = "Ore Generation";
+    public static final String SUBCATEGORY_TIN = "Tin Ore";
+    public static final String SUBCATEGORY_ALUMINUM = "Aluminum Ore";
+    public static final String SUBCATEGORY_COPPER = "Copper Ore";
+    public static final String SUBCATEGORY_RUBY = "Ruby Ore";
+    public static final String SUBCATEGORY_LEAD = "Lead Ore";
+    public static ForgeConfigSpec spec;
     public static ForgeConfigSpec.BooleanValue tin_gen;
     public static ForgeConfigSpec.IntValue tin_chance;
-    public static ForgeConfigSpec.IntValue tin_veinsize;
+    public static ForgeConfigSpec.IntValue tin_min;
     public static ForgeConfigSpec.IntValue tin_max;
-    
-    public static ForgeConfigSpec.BooleanValue aluminum_gen;
-    public static ForgeConfigSpec.IntValue aluminum_chance;
-    public static ForgeConfigSpec.IntValue aluminum_veinsize;
-    public static ForgeConfigSpec.IntValue aluminum_max;
-    
+    public static ForgeConfigSpec.BooleanValue alum_gen;
+    public static ForgeConfigSpec.IntValue alum_chance;
+    public static ForgeConfigSpec.IntValue alum_min;
+    public static ForgeConfigSpec.IntValue alum_max;
     public static ForgeConfigSpec.BooleanValue copper_gen;
     public static ForgeConfigSpec.IntValue copper_chance;
-    public static ForgeConfigSpec.IntValue copper_veinsize;
+    public static ForgeConfigSpec.IntValue copper_min;
     public static ForgeConfigSpec.IntValue copper_max;
-    
     public static ForgeConfigSpec.BooleanValue ruby_gen;
     public static ForgeConfigSpec.IntValue ruby_chance;
-    public static ForgeConfigSpec.IntValue ruby_veinsize;
+    public static ForgeConfigSpec.IntValue ruby_min;
     public static ForgeConfigSpec.IntValue ruby_max;
-    
     public static ForgeConfigSpec.BooleanValue lead_gen;
     public static ForgeConfigSpec.IntValue lead_chance;
-    public static ForgeConfigSpec.IntValue lead_veinsize;
+    public static ForgeConfigSpec.IntValue lead_min;
     public static ForgeConfigSpec.IntValue lead_max;
     
-    public static final String ORES = "Ores";
-    
-    public static void init() {
-    	//Client config
-    	ForgeConfigSpec.Builder clientBuilder = new ForgeConfigSpec.Builder();
-    	clientSpec = clientBuilder.build();
-		ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, clientSpec);
-		
-		//Server config
-		ForgeConfigSpec.Builder serverBuilder = new ForgeConfigSpec.Builder();
-		serverBuilder.push(ORES);
-		
-		tin_gen = serverBuilder.comment("Generate this ore?").translation("config.clib.tin_gen").define("tin_gen", true);
-		tin_chance = serverBuilder.comment("Attempts to spawn per chunk").translation("config.clib.tin_gen").defineInRange("tin_chance", 20, 1, 100);
-		tin_max = serverBuilder.comment("Maximum height ore can spawn").translation("config.clib.tin_gen").defineInRange("tin_max", 60, 1, 255);
-		tin_veinsize = serverBuilder.comment("Size of spawned veins").translation("config.clib.tin_gen").defineInRange("tin_veinsize", 8, 1, 20);
-		
-		aluminum_gen = serverBuilder.comment("Generate this ore?").translation("config.clib.aluminum_gen").define("aluminum_gen", true);
-		aluminum_chance = serverBuilder.comment("Attempts to spawn per chunk").translation("config.clib.aluminum_gen").defineInRange("aluminum_chance", 15, 1, 100);
-		aluminum_max = serverBuilder.comment("Maximum height ore can spawn").translation("config.clib.aluminum_gen").defineInRange("aluminum_max", 60, 1, 255);
-		aluminum_veinsize = serverBuilder.comment("Size of spawned veins").translation("config.clib.aluminum_gen").defineInRange("aluminum_veinsize", 8, 1, 20);
-		
-		copper_gen = serverBuilder.comment("Generate this ore?").translation("config.clib.copper_gen").define("copper_gen", true);
-		copper_chance = serverBuilder.comment("Attempts to spawn per chunk").translation("config.clib.copper_gen").defineInRange("copper_chance", 20, 1, 100);
-		copper_max = serverBuilder.comment("Maximum height ore can spawn").translation("config.clib.copper_gen").defineInRange("copper_max", 60, 1, 255);
-		copper_veinsize = serverBuilder.comment("Size of spawned veins").translation("config.clib.copper_gen").defineInRange("copper_veinsize", 8, 1, 20);
-		
-		ruby_gen = serverBuilder.comment("Generate this ore?").translation("config.clib.ruby_gen").define("ruby_gen", true);
-		ruby_chance = serverBuilder.comment("Attempts to spawn per chunk").translation("config.clib.ruby_gen").defineInRange("ruby_chance", 4, 1, 100);
-		ruby_max = serverBuilder.comment("Maximum height ore can spawn").translation("config.clib.ruby_gen").defineInRange("ruby_max", 18, 1, 255);
-		ruby_veinsize = serverBuilder.comment("Size of spawned veins").translation("config.clib.ruby_gen").defineInRange("ruby_veinsize", 3, 1, 20);
-		
-		lead_gen = serverBuilder.comment("Generate this ore?").translation("config.clib.lead_gen").define("lead_gen", true);
-		lead_chance = serverBuilder.comment("Attempts to spawn per chunk").translation("config.clib.lead_gen").defineInRange("lead_chance", 12, 1, 100);
-		lead_max = serverBuilder.comment("Maximum height ore can spawn").translation("config.clib.lead_gen").defineInRange("lead_max", 60, 1, 255);
-		lead_veinsize = serverBuilder.comment("Size of spawned veins").translation("config.clib.lead_gen").defineInRange("lead_veinsize", 8, 1, 20);
-		
-		serverBuilder.pop();
-		serverSpec = serverBuilder.build();
-		ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, serverSpec);
+    @SubscribeEvent
+    public static void onLoad(final ModConfig.Loading configEvent) {
     }
     
-    public static void load(){
-		CommentedFileConfig clientConfig = CommentedFileConfig.builder(FMLPaths.CONFIGDIR.get().resolve(ConguerorLib.MOD_ID + "-client.toml")).sync().autosave().writingMode(WritingMode.REPLACE).build();
-		clientConfig.load();
-		clientSpec.setConfig(clientConfig);
-
-		CommentedFileConfig serverConfig = CommentedFileConfig.builder(FMLPaths.CONFIGDIR.get().resolve(ConguerorLib.MOD_ID + "-server.toml")).sync().autosave().writingMode(WritingMode.REPLACE).build();
-		serverConfig.load();
-		serverSpec.setConfig(serverConfig);
-	}
+    @SubscribeEvent
+    public static void onReload(final ModConfig.Reloading configEvent) {
+    }
+    
+    static {
+        final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
+        BUILDER.comment("General Config").push("General");
+        BUILDER.pop();
+        BUILDER.comment("Ore Generation Settings").push("Ore Generation");
+        BUILDER.comment("Aluminum Settings").push("Aluminum Ore");
+        ClibConfig.alum_gen = BUILDER.comment("Set to false to disable spawns [default=true]").translation("clib.config.alum.generate").define("alum_gen", true);
+        ClibConfig.alum_chance = BUILDER.comment("Maximum number of ore veins per chunk (set 0 to disable spawns) [deafult=15]").translation("clib.config.alum.chance").defineInRange("alum_chance", 20, 0, 50);
+        ClibConfig.alum_min = BUILDER.comment("Minimum y value the ore can generate at (must always be lower than maximum value) [default=5]").translation("clib.config.alum.min").defineInRange("alum_min", 0, 0, 255);
+        ClibConfig.alum_max = BUILDER.comment("Maximum y value the ore can generate at (must always be higher than minimum value) [default=60]").translation("clib.config.alum.max").defineInRange("alum_max", 60, 0, 255);
+        BUILDER.pop();
+        BUILDER.comment("Tin Settings").push("Tin Ore");
+        ClibConfig.tin_gen = BUILDER.comment("Set to false to disable spawns [default=true]").translation("clib.config.tin.generate").define("tin_gen", true);
+        ClibConfig.tin_chance = BUILDER.comment("Maximum number of ore veins per chunk (set 0 to disable spawns) [deafult=15]").translation("clib.config.tin.chance").defineInRange("tin_chance", 15, 0, 50);
+        ClibConfig.tin_min = BUILDER.comment("Minimum y value the ore can generate at (must always be lower than maximum value) [default=5]").translation("clib.config.tin.min").defineInRange("tin_min", 0, 0, 255);
+        ClibConfig.tin_max = BUILDER.comment("Maximum y value the ore can generate at (must always be higher than minimum value) [default=60]").translation("clib.config.tin.max").defineInRange("tin_max", 60, 0, 255);
+        BUILDER.pop();
+        BUILDER.comment("Copper Settings").push("Copper Ore");
+        ClibConfig.copper_gen = BUILDER.comment("Set to false to disable spawns [default=true]").translation("clib.config.copper.generate").define("copper_gen", true);
+        ClibConfig.copper_chance = BUILDER.comment("Maximum number of ore veins per chunk (set 0 to disable spawns) [deafult=15]").translation("clib.config.copper.chance").defineInRange("copper_chance", 15, 0, 50);
+        ClibConfig.copper_min = BUILDER.comment("Minimum y value the ore can generate at (must always be lower than maximum value) [default=5]").translation("clib.config.copper.min").defineInRange("copper_min", 0, 0, 255);
+        ClibConfig.copper_max = BUILDER.comment("Maximum y value the ore can generate at (must always be higher than minimum value) [default=60]").translation("clib.config.copper.max").defineInRange("copper_max", 60, 0, 255);
+        BUILDER.pop();
+        BUILDER.comment("Ruby Settings").push("Ruby Ore");
+        ClibConfig.ruby_gen = BUILDER.comment("Set to false to disable spawns [default=true]").translation("clib.config.ruby.generate").define("ruby_gen", true);
+        ClibConfig.ruby_chance = BUILDER.comment("Maximum number of ore veins per chunk (set 0 to disable spawns) [deafult=15]").translation("clib.config.ruby.chance").defineInRange("ruby_chance", 3, 0, 50);
+        ClibConfig.ruby_min = BUILDER.comment("Minimum y value the ore can generate at (must always be lower than maximum value) [default=5]").translation("clib.config.ruby.min").defineInRange("ruby_min", 8, 0, 255);
+        ClibConfig.ruby_max = BUILDER.comment("Maximum y value the ore can generate at (must always be higher than minimum value) [default=60]").translation("clib.config.ruby.max").defineInRange("ruby_max", 20, 0, 255);
+        BUILDER.pop();
+        BUILDER.comment("Lead Settings").push("Lead Ore");
+        ClibConfig.lead_gen = BUILDER.comment("Set to false to disable spawns [default=true]").translation("clib.config.lead.generate").define("lead_gen", true);
+        ClibConfig.lead_chance = BUILDER.comment("Maximum number of ore veins per chunk (set 0 to disable spawns) [deafult=15]").translation("lead_chance_comment").defineInRange("lead_chance", 3, 0, 50);
+        ClibConfig.lead_min = BUILDER.comment("Minimum y value the ore can generate at (must always be lower than maximum value) [default=5]").translation("lead_min_comment").defineInRange("lead_min", 8, 0, 255);
+        ClibConfig.lead_max = BUILDER.comment("Maximum y value the ore can generate at (must always be higher than minimum value) [default=60]").translation("lead_max_comment").defineInRange("lead_max", 20, 0, 255);
+        BUILDER.pop();
+        BUILDER.pop();
+        ClibConfig.spec = BUILDER.build();
+    }
 }
